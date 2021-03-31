0f49071e42f3525036132f2c53db721aa19ac1cc
Sandra Wazaz <sandrawazaz@gmail.com>
2021-03-31 08:55:28 -0400

    edit

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+let welcomeBtn = false
+
 const url = 'http://localhost:3000/drawings'
 const collection = document.querySelector('div#drawing-collection')
+const usernameForm = document.querySelector('form#username-form')
+const drawDiv = document.querySelector('div#all-drawings')
+const welcomeDiv = document.querySelector('div#welcome')
+
+usernameForm.addEventListener("submit", (event) => {
+
+  event.preventDefault()
+  // hide & seek with the form
+  welcomeBtn = !welcomeBtn
+  if (welcomeBtn) {
+    drawDiv.style.display = "none"
+  } else {
+    drawDiv.style.display = "block"
+  }
+})
+
+function hideWelcome () {
+  if (drawDiv.style.display === "block"){
+    welcomeDiv.style.display = "none"
+  }
+}
+
+hideWelcome()
+
 
 function renderAllDrawings() {
     fetch(url)
